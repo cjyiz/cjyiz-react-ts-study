ac77c9c8cbd6e6b1d731f79fe94f7434764b92ad
chenji <cjyiz@aliyun.com>
2020-11-15 18:36:46 +0800

    feat: s

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 // import DomToImg from "./components/DomToImg/index";
 // import HtmlToImg from "./components/htmlToImg.tsx";
-// import Count from "./components/Count";
+import Count from "./components/Count";
 // import Mouse from "./components/Position";
 // import useMousePosition from "./hooks/useMousePosition";
 import useUrlLoader from "./hooks/useUrlLoader";
@@ -10,6 +10,20 @@ interface IShowResult {
   message: string;
   status: string;
 }
+interface IThemenProps {
+  [key: string]: { color: string; background: string };
+}
+const themes: IThemenProps = {
+  light: {
+    color: "#000",
+    background: "#eee",
+  },
+  dark: {
+    color: "#fff",
+    background: "#000",
+  },
+};
+export const ThemeContext = React.createContext(themes.light);
 // import domtoimage from 'dom-to-image';
 const App: React.FC = () => {
   // const node = document.getElementById('poster')
@@ -18,9 +32,10 @@ const App: React.FC = () => {
   // })
   const [show, setShow] = useState(true);
   // 这里第二个参数就是用来监控他变化的
-  const [data, loading] = useUrlLoader(
-    "https://dog.ceo/api/breeds/image/random",[show]
-  );
+  const [
+    data,
+    loading,
+  ] = useUrlLoader("https://dog.ceo/api/breeds/image/random", [show]);
   const dogUrl = data as IShowResult;
   // const position = useMousePosition();
   useEffect(() => {
@@ -30,18 +45,21 @@ const App: React.FC = () => {
     <div className="App">
       {/* <div className='cjyiz' id='poster'>
       </div> */}
-      {/* <Count /> */}
-      <button
-        onClick={() => {
-          setShow(!show);
-        }}
-      >
-        点击切换图片
-      </button>
-      {/* <Mouse /> */}
-      {loading ? <p>狗狗读取中</p> : <img src={dogUrl && dogUrl.message} />}
-      {/* <HtmlToImg /> */}
-      {/* <DomToImg /> */}
+      <ThemeContext.Provider value={themes.dark}>
+        <Count />
+      </ThemeContext.Provider>
+
+        {/* <button
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          点击切换图片
+        </button> */}
+        {/* <Mouse /> */}
+        {/* {loading ? <p>狗狗读取中</p> : <img src={dogUrl && dogUrl.message} />} */}
+        {/* <HtmlToImg /> */}
+        {/* <DomToImg /> */}
     </div>
   );
 };
--- src/components/Count/index.tsx
+++ src/components/Count/index.tsx
@@ -1,28 +1,54 @@
-import React,{useEffect, useState} from 'react'
-import useMousePosition from '../../hooks/useMousePosition'
-import useUrlLoader from '../../hooks/useUrlLoader'
-interface IShowResult{
-    message:string;
-    status:string;
+import React, { useEffect, useState, useContext, useRef } from "react";
+import useMousePosition from "../../hooks/useMousePosition";
+import useUrlLoader from "../../hooks/useUrlLoader";
+import {ThemeContext} from "../../App";
+interface IShowResult {
+  message: string;
+  status: string;
 }
-const Count:React.FC=()=>{
-    const [obj,setObj]=useState({like:0,on:true})
-    const ss=useMousePosition()
-    const [data,loading]=useUrlLoader('https://dog.ceo/api/breeds/image/random')
-    const dogUrl=data as IShowResult
-    useEffect(()=>{
-        console.log('点击执行')
-        document.title=`点击了${obj.like}次`
-    },[obj.like])
-    return <div>
-        <button onClick={()=>{
-            setObj({like:obj.like+1,on:obj.on})
-        }}>{obj.like}👍</button>
-        <p>{ss.x}</p>
-        {loading?<p>购过读取中</p>:<img src={dogUrl&&dogUrl.message} />}
-         <button onClick={()=>{
-            setObj({like:obj.like+1,on:!obj.on})
-        }}>{obj.on?'on':"off"}👍</button>
+const Count: React.FC = () => {
+  const [obj, setObj] = useState({ like: 0, on: true });
+  // 在render里面中只有一个值
+  const likeRef = useRef(0);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const ss = useMousePosition();
+  const [data, loading] = useUrlLoader(
+    "https://dog.ceo/api/breeds/image/random"
+  );
+  const dogUrl = data as IShowResult;
+  const theme = useContext(ThemeContext);
+ 
+  useEffect(() => {
+    console.log("点击执行");
+    document.title = `点击了${obj.like}次`;
+  }, [obj.like]);
+  useEffect(() => {
+    if (inputRef && inputRef.current) {
+      inputRef.current.focus();
+    }
+  });
+  return (
+    <div>
+      <button
+      style={{color:theme.color,background:theme.background}}
+        onClick={() => {
+          setObj({ like: obj.like + 1, on: obj.on });
+        }}
+      >
+        {obj.like}👍
+      </button>
+      <p>{ss.x}</p>
+      {loading ? <p>购过读取中</p> : <img src={dogUrl && dogUrl.message} />}
+      <button
+        onClick={() => {
+          setObj({ like: obj.like + 1, on: !obj.on });
+        }}
+      >
+        {obj.on ? "on" : "off"}👍
+      </button>
+
+      <input type="text" ref={inputRef} />
     </div>
-}
-export default Count
+  );
+};
+export default Count;
