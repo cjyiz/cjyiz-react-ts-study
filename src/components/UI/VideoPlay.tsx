20ecbb33331f422cf11e6256e52469910d66e932
chenji <cjyiz@aliyun.com>
2021-03-17 17:09:36 +0800

    feat: npm link配置

--- src/components/UI/VideoPlay.tsx
+++ src/components/UI/VideoPlay.tsx
@@ -1,7 +1,12 @@
 import React, { FC, useEffect, useState } from "react";
-import { VideoPlayer, Button } from "tezign-ui";
-// import 'tezign-ui/lib/video-player/style/index'; 
+// import { VideoPlayer, Button } from "tezign-ui";
+// import 'tezign-ui/lib/video-player/style/index';
 // import 'tezign-ui/lib/video-player/style/css'
+// import "./index.css";
+const tezignUI = require("tezign-ui");
+require('tezign-ui/lib/video-player/style/css')
+console.log("新加载的模块", tezignUI);
+const { VideoPlayer } = tezignUI;
 const state = {
   source: "https://video.tezign.com/home/tezign_brand_video_zh.mp4",
   poster:
@@ -17,11 +22,14 @@ export const CjVideoPlayer: FC = () => {
   }, []);
   return (
     <div>
+      {/* <video src={source} controls width='300'> </video> */}
       <VideoPlayer
         source={source}
         poster={poster}
         download="Test Video"
+        height="500px"
         fit="flex"
+        className="cc"
       />
       <br />
       <br />
