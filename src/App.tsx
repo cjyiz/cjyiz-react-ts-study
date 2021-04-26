92b57784fd77c453f84b687984f2babe3519d86d
chenji <cjyiz@aliyun.com>
2021-04-26 16:18:34 +0800

    feat: 虚拟列表

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState, useCallback } from "react";
 import "./App.css";
 import { CUseContext } from "@/components/Study/useContext/CUseContext";
 // import { CjText } from "./算法/test.js";
+import { VisualDom } from "@/components/UI/VisualDom/VisualDom";
 function App() {
   return (
     <div className="cjyiz">
-      <CUseContext />
+      <VisualDom />
     </div>
   );
 }
